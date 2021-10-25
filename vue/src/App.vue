af5a318787b058756c490b0c0609abf8753f6a1a
서민혁 <mahns1201@gmail.com>
2021-10-25 23:39:41 +0900

    [init] vue3
    + new project

---
+++ vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Home />
+</template>
+
+<script>
+import Home from "../src/views/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style></style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
