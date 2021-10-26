773f5dd6672e49f81b5aff75ce56a72ff1fff120
서민혁 <mahns1201@gmail.com>
2021-10-26 23:59:47 +0900

    [add] app
    + layout

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -10,4 +10,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  /* layout */
+  position: absolute;
+  left: 10%;
+  height: 100vh;
+  width: 80%;
+}
+
+* {
+  /* padding, margin default */
+  padding: 0;
+  margin: 0;
+
+  /* font */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
