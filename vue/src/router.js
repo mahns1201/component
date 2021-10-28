b454b60342ad4b07f98334efc0c1999967a3f0c6
서민혁 <mahns1201@gmail.com>
2021-10-28 19:21:36 +0900

    [feat] 404 page route
    + NotFound 페이지
    + redirect

--- vue/src/router.js
+++ vue/src/router.js
@@ -1,18 +1,28 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "./views/Home.vue";
+import NotFound from "./views/NotFound.vue";
 import InputTag from "./components/InputTag.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home, // 동적 import
+    component: Home,
   },
   {
     path: "/input-tag",
     name: "InputTag",
     component: InputTag,
+  },
+  {
+    path: "/404",
+    name: "notFound",
+    component: NotFound,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/404",
   },
 ];
 
@@ -20,4 +30,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
 export default router;
